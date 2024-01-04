d55d4d8561bdbcfac62ae295ac87258c046d27c1
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2024-01-04 13:48:29 -0500

    Updated main.js
    added function labeled as: "game" and added loop and the above "playRound function"

--- js/main.js
+++ js/main.js
@@ -9,8 +9,18 @@ function getComputerChoice() {
 
 // Step #2 - Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
+function playRound(playerSelection, computerSelection) {}
+
 // Bonus Points: Make your function’s playerSelection parameter case-insensitive.
 
 // Bonus Points: Account for TIES by re-playing the round.
 
 // Step #3 - Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
+
+function game() {
+  const playerWinCount = 0;
+  const computerWinCount = 0;
+  do {
+    playRound();
+  } while (playerWinCount <= 5 || computerWinCount <= 5);
+}
