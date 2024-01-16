b5b6c9196e39df793f9060d23c58e54ff6f506a6
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2024-01-15 21:44:51 -0500

    updated main.js
    needs further in-depth review

--- js/main.js
+++ js/main.js
@@ -4,30 +4,60 @@ function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   let num = Math.floor(Math.random() * 3);
   console.log(num);
-  return choices(num);
+  return choices[num].toLowerCase();
 }
 
 // Step #2 - Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
 function playRound(playerSelection, computerSelection) {
-  switch (key) {
-    case value:
+  let askUser = prompt(
+    "Please choose one of the following options: Rock, Paper or Scissors"
+  );
+  playerSelection = askUser.toLowerCase(); // Bonus Points: Make your function’s playerSelection parameter case-insensitive.
+
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "rock") {
+        console.log("It's a tie, play again!");
+        playRound(); // Bonus Points: Account for TIES by re-playing the round.
+      } else if (computerSelection === "paper") {
+        console.log("You lose! Paper beats Rock, try again.");
+        ++computerWinCount;
+      } else if (computerSelection === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        ++playerWinCount;
+      }
       break;
-    case value:
+
+    case "paper":
+      if (computerSelection === "paper") {
+        console.log("It's a tie, play again!");
+        playRound(); // Bonus Points: Account for TIES by re-playing the round.
+      } else if (computerSelection === "scissors") {
+        console.log("You lose! Scissors beat Paper, try again.");
+        ++computerWinCount;
+      } else if (computerSelection === "rock") {
+        console.log("You win! Paper beats Rock.");
+        ++playerWinCount;
+      }
       break;
-    case value:
-      break;
-    default:
+
+    case "scissors":
+      if (computerSelection === "scissors") {
+        console.log("It's a tie, play again!");
+        playRound(); // Bonus Points: Account for TIES by re-playing the round.
+      } else if (computerSelection === "paper") {
+        console.log("You win! Scissors beats Paper.");
+        ++playerWinCount;
+      } else if (computerSelection === "rock") {
+        console.log("You lose! Rock beats Scissors.");
+        ++computerWinCount;
+      }
       break;
   }
 }
 
-// Bonus Points: Make your function’s playerSelection parameter case-insensitive.
-
-// Bonus Points: Account for TIES by re-playing the round.
-
 // Step #3 - Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
-
 function game() {
   const playerWinCount = 0;
   const computerWinCount = 0;
